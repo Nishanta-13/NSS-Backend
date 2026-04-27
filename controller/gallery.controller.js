c0191513cfdc7f8f67f21a27eb9260e094453d18
Nishanta Kamal Baruah <nishantapro@gmail.com>
2026-04-28 00:22:14 +0530

    Change in hashtag

--- controller/gallery.controller.js
+++ controller/gallery.controller.js
@@ -37,7 +37,7 @@ const receiveInstagramPost = async (req, res) => {
       return res.status(400).json({ error: "No media data" });
     }
 
-    const allowedHashtags = ["#NSS", "#Event"];
+    const allowedHashtags = ["#NSS", "#nssnits", "#nss"];
 
     const captionText = caption?.toLowerCase() || "";
 
